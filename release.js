f5cf7d46d03bbc23d04e8629f923d4725bbeb806
Fabian Mieller <kontakt@fabianmieller.de>
2019-02-25 10:08:31 +0100

    update

--- release.js
+++ release.js
@@ -1,13 +1,8 @@
 const version = require('./package.json').version;
-// const simpleGit = require('simple-git')();
 const replace = require('replace-in-file');
-
-console.log(version);
 
 replace({
     files: 'README.md',
     from: /(\/|@|Version: |\/v)\d+\.\d+\.\d+/g,
     to: '$1' + version
 });
-
-// simpleGit.add('README.md').commit(version).push(['-u', 'origin', 'develop']);
